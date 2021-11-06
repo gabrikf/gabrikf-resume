f06173102db1bc01eb30bc0b3e133ebb74728860
Gabriel Koch Fodi <gabrielkochfodi@gmail.com>
2021-11-06 15:33:55 -0300

    updating content

--- components/Education/EducationItem.js
+++ components/Education/EducationItem.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const EducationItem = () => {
   return (
-    <div className='bg-grey rounded-lg shadow-lg py-6 px-12 leading-none ml-8 md:ml-0'>
-      <h4 className='text-lg uppercase font-bold mb-2 text-red-800'>Courses</h4>
-      <p className='text-lg md:text-2xl uppercase'>FullStack Master<br />
-      <span className='text-lg normal-case font-bold'>Dev-Pleno - Tulio Faria</span></p>
-    </div>
-  )
-}
+    <>
+      <div className="bg-grey rounded-lg shadow-lg py-6 px-12 leading-none ml-8 md:ml-0">
+        <h4 className="text-lg uppercase font-bold mb-2 text-red-800">
+          Courses
+        </h4>
+        <div className-="flex flex-row">
+          <div>
+            <p className="text-lg md:text-2xl uppercase">
+              FullStack Master
+              <br />
+              <span className="text-lg normal-case font-bold">
+                Dev-Pleno - Tulio Faria
+              </span>
+            </p>
+          </div>
 
-export default EducationItem
+          <div>
+            <p className="text-lg md:text-2xl uppercase">
+              FullStack Master
+              <br />
+              <span className="text-lg normal-case font-bold">
+                Dev-Pleno - Tulio Faria
+              </span>
+            </p>
+          </div>
+        </div>
+
+        <h4 className="text-lg uppercase font-bold mb-2 text-red-800">
+          College
+        </h4>
+        <p className="text-lg md:text-2xl uppercase">
+          Análise e Desenvolvimento de Sistemas
+          <br />
+          <span className="text-lg normal-case font-bold">Senac - SC</span>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default EducationItem;
--- components/Summary.js
+++ components/Summary.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Summary = () => {
   return (
-    <div className='bg-grey rounded-lg shadow-lg pt-2 pb-4 md:pb-12 px-8 mr-8 md:mr-0 md:px-14'>
-    <h3 className='text-4xl text-center text-red-800 uppercase'>My Stack</h3>
-    <p className='md:text-2xl text-1xl font-bold md:text-center '>Javascript / NodeJs / React / HTML / CSS / Bootstrap / Next / Axios / Express / MySQL / Firestore / SQLite / Tailwind</p>
-  </div>
-  )
-}
+    <div className="bg-grey rounded-lg shadow-lg pt-2 pb-4 md:pb-12 px-8 mr-8 md:mr-0 md:px-14">
+      <h3 className="text-4xl text-center text-red-800 uppercase">My Stack</h3>
+      <p className="md:text-2xl text-1xl font-bold md:text-center ">
+        Javascript / Typescript / NodeJs / React / Next / HTML / CSS / Axios /
+        Express / MySQL / Tailwind
+      </p>
+    </div>
+  );
+};
 
-export default Summary
+export default Summary;
