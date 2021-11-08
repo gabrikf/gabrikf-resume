9a7b96c2d0a734d0b298db913108e4d80db643b1
Gabriel Koch Fodi <67654630+gabrikf@users.noreply.github.com>
2021-11-08 09:16:56 -0300

    making icons change color when hover

--- components/Hero/ContactMe.js
+++ components/Hero/ContactMe.js
@@ -7,8 +7,8 @@ const ContactMe = () => {
     <div className='mr-20 mx-8 px-8 relative border-red-800 border-2 rounded md:px-16 pb-4 pt-10 mt-6'>
           <h3 className='absolute bg-red-800 py-2 px-6 text-white top-0 -mt-5 text-2xl font-bold uppercase'>Contact me</h3>
           <p>
-            <a href='https://www.linkedin.com/in/gabriel-koch-fodi-36b12b8b/'><FaLinkedin className='text-6xl inline-block mr-6 ml-6'/></a>
-            <a href='https://github.com/gabrikf'><FaGithub  className='text-6xl inline-block '/></a>
+            <a href='https://www.linkedin.com/in/gabriel-koch-fodi-36b12b8b/'><FaLinkedin className='text-6xl hover:text-blue-600 inline-block mr-6 ml-6'/></a>
+            <a href='https://github.com/gabrikf'><FaGithub  className='text-6xl hover:text-purple-600 inline-block '/></a>
             <br />
             <span className='inline-block mt-4'> or drop a line: gabrielkochfodi@gmail.com</span>
           </p>
